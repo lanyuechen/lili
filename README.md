4e546b803c6e85f00af30afbc3b812b1a29045bf
zhenglipeng <zhenglipeng@sensetime.com>
2023-05-12 14:22:52 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# lili
+
+demo
+
+## Features
+
+- feature1
+
+## Requirements
+
+- none
+
+## Extension Settings
+
+- none
+
+## Known Issues
+
+- none
+
+## Release Notes
+
+- none
---
+++ src/panels/HelloPanel.ts
@@ -0,0 +1,88 @@
+import * as vscode from 'vscode';
+
+import { getUri } from '../utils/getUri'; 
+import { getNonce } from '../utils/getNonce';
+
+export class HelloPanel {
+  public static currentPanel: HelloPanel | undefined;
+  private readonly _panel: vscode.WebviewPanel;
+  private _disposables: vscode.Disposable[] = [];
+
+  private constructor(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
+    this._panel = panel;
+
+    this._panel.webview.html = this._getWebviewContent(this._panel.webview, extensionUri);
+
+    this._panel.onDidDispose(() => this.dispose(), null, this._disposables);
+    
+    this._setWebviewMessageListener(this._panel.webview);
+  }
+
+  private _setWebviewMessageListener(webview: vscode.Webview) {
+    webview.onDidReceiveMessage(
+      (message: any) => {
+        const command = message.command;
+        const text = message.text;
+
+        switch (command) {
+          case "hello":
+            vscode.window.showInformationMessage(text);
+            return;
+        }
+      },
+      undefined,
+      this._disposables
+    );
+  }
+
+  private _getWebviewContent(webview: vscode.Webview, extensionUri: vscode.Uri) {
+    const webviewUri = getUri(webview, extensionUri, ['out', 'webview.js']);
+
+    const nonce = getNonce();
+
+    return /*html*/ `
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; font-src ${webview.cspSource}; img-src ${webview.cspSource} https:; script-src 'nonce-${nonce}';">
+          <title>Hello World!</title>
+        </head>
+        <body>
+          <h1>Hello World!</h1>
+          <vscode-button id="howdy">Howdy!</vscode-button>
+          <script type="module" nonce="${nonce}" src="${webviewUri}"></script>
+        </body>
+      </html>
+    `;
+  }
+
+  public dispose() {
+    HelloPanel.currentPanel = undefined;
+
+    this._panel.dispose();
+
+    while (this._disposables.length) {
+      const disposable = this._disposables.pop();
+      if (disposable) {
+        disposable.dispose();
+      }
+    }
+  }
+
+  public static render(extensionUri: vscode.Uri) {
+    if (HelloPanel.currentPanel) {
+      HelloPanel.currentPanel._panel.reveal(vscode.ViewColumn.One);
+    } else {
+      const panel = vscode.window.createWebviewPanel("hello-world", "Hello World", vscode.ViewColumn.One, {
+        // Enable javascript in the webview
+        enableScripts: true,
+        // Restrict the webview to only load resources from the `out` directory
+        localResourceRoots: [vscode.Uri.joinPath(extensionUri, 'out')],
+      });
+
+      HelloPanel.currentPanel = new HelloPanel(panel, extensionUri);
+    }
+  }
+}
